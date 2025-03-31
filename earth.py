babca8e80df6c7dc6d8915dfc75725206ca6012b
abril1208 <arabinovich@mail.austral.edu.ar>
2025-03-31 18:39:04 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,10 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    if x<y:
+        print("The result of "+ x + " comes first in the dictionary than " +y + " is True.")
+        print("The result of "+ y + " comes first in the dictionary than " +x + " is False.")
+    elif x>y:
+       print("The result of "+ x + " comes first in the dictionary than " +y + " is False.")
+       print("The result of "+ y + " comes first in the dictionary than " +x + " is True.")
+earth()
